5e23363b6b8d7aefbfb46b7ec4d990202c380207
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-01-04 04:09:38 -0300

    Fix validation, verification and make other improvements

--- cofre.c
+++ cofre.c
@@ -3,56 +3,83 @@
 #include <string.h>
 
 #define PASSWORD_LENGTH 3
+#define INPUT_LENGTH PASSWORD_LENGTH * 2 + 1
 
 int validate_input(char* input);
 unsigned short* get_password(char* input);
 int verify_password(const unsigned short* target_password, unsigned short* password);
+void flush_stdin();
 
 int main() {
     const unsigned short VAULT_PASSWORD[PASSWORD_LENGTH] = {1, 0, 6};
-    char input[PASSWORD_LENGTH * 2];
+    char* input = malloc(INPUT_LENGTH);
     unsigned short* password;
+    unsigned short validation_result;
 
-    printf("Digite a senha: ");
-    fgets(input, PASSWORD_LENGTH * 2, stdin);
+    while (1) {
+        printf("Digite a combinação de %hu dígitos para abrir o cofre: ", PASSWORD_LENGTH);
+        fgets(input, INPUT_LENGTH, stdin);
+        printf("\n");
 
-    if (input[strlen(input) - 1] == '\n') {
-        input[strlen(input) - 1] = '\0';
+        validation_result = validate_input(input);
+        
+        if (validation_result) {
+            printf("Entrada inválida. Tente novamente.\n\n");
+            if (validation_result == 1) flush_stdin();
+            continue;
+        }
+
+        if (input[strlen(input) - 1] == '\n') {
+            input[strlen(input) - 1] = '\0';
+        }
+
+        password = get_password(input);
+
+        if (!verify_password(VAULT_PASSWORD, password)) {
+            break;
+        }
     }
-
-    if (validate_input(input)) {
-        printf("Entrada inválida. Tente novamente.\n");
-        exit(1);
-    }
-
-    password = get_password(input);
-
-    printf("Senha %s\n", verify_password(VAULT_PASSWORD, password) ? "incorreta" : "correta");
+        
+    printf("Parabéns! Você abriu o cofre!\n");
 
     return 0;
 }
 
+// Function to validate the user input
 int validate_input(char* input) {
-    if (strlen(input) != PASSWORD_LENGTH * 2 - 1) {
+    // Check if the last non-null character is a new line
+    if (!strchr(input, '\n')) {
         return 1;
     }
 
+    // Check if the input length is incorrect
+    if (strlen(input) < PASSWORD_LENGTH * 2) {
+        return 2;
+    }
+
+    // Check if the characters are correct
     for (unsigned short i = 0; i < strlen(input); i++) {
+        // Check if characters in even indexes are numeric or not
         if (i % 2 == 0 && (input[i] < 48 || input[i] > 57)) {
-            return 1;
-        } else if (i % 2 != 0 && input[i] != ' ') {
-            return 1;
+            return 3;
+        // Check if characters in odd indexes are whitespace or not
+        } else if (i % 2 != 0 && input[i] != ' ' && input[i] != '\n') {
+            return 4;
         }
     }
 
     return 0;
 }
 
+// Function to convert the user input to an array that represents the password
 unsigned short* get_password(char* input) {
+    // Declare password variable
     unsigned short* password;
 
+    // Initialize password variable
     password = calloc(PASSWORD_LENGTH, sizeof(password));
 
+    // Convert numeric characters to numbers
     for (unsigned short i = 0; i < strlen(input); i++) {
         if (i % 2 == 0) {
             password[i / 2] = input[i] - 48;
@@ -62,39 +89,66 @@ unsigned short* get_password(char* input) {
     return password;
 }
 
+// Function to check if the password is correct
 int verify_password(const unsigned short* target_password, unsigned short* password) {
-    unsigned short correct_digits = 0;
-    unsigned short correct_positions[PASSWORD_LENGTH];
-    int skip = 0;
+    // Declare arrays
+    int correct_digits[PASSWORD_LENGTH];
+    int uncovered_digits[PASSWORD_LENGTH];
+    int wrong_position_digits[PASSWORD_LENGTH];
 
+    // Initialize values
     for (unsigned short i = 0; i < PASSWORD_LENGTH; i++) {
-        correct_positions[i] = 0;
-    } 
+        correct_digits[i] = 0;
+        uncovered_digits[i] = 0;
+        wrong_position_digits[i] = 0;
+    }
 
+    // Check if there are correct digits
     for (unsigned short i = 0; i < PASSWORD_LENGTH; i++) {
         if (password[i] == target_password[i]) {
-            printf("O dígito %hu está correto na posição %u.\n", password[i], i + 1);
-            correct_digits++;
-            correct_positions[i] = 1;
-            continue;
+            correct_digits[i] = 1;
         }
+    }
 
-        for (unsigned short j = 0; j < PASSWORD_LENGTH; j++) {
-            if (correct_positions[j] == 0 && i != j && password[i] == target_password[j]) {
-                printf("O dígito %hu está correto, mas na posição errada.\n", password[i]);
-                skip = 1;
-                break;
+    // Check if there are correct digits but in the wrong position
+    for (unsigned short i = 0; i < PASSWORD_LENGTH; i++) {
+        for (unsigned j = 0; j < PASSWORD_LENGTH; j++) {
+            if (i == j || correct_digits[i] || correct_digits[j] || uncovered_digits[j]) {
+                continue;
+            }
+
+            if (password[i] == target_password[j]) {
+                wrong_position_digits[i] = 1;
+                uncovered_digits[j] = 1;
             }
         }
-
-        if (!skip) printf("O dígito %hu não faz parte da senha.\n", password[i]);
     }
 
-    printf("\nCorrects: %hu\n", correct_digits);
-
-    if (correct_digits == PASSWORD_LENGTH) {
-        return 0;
+    // Give feedback to the user
+    for (unsigned short i = 0; i < PASSWORD_LENGTH; i++) {
+        if (correct_digits[i]) {
+            printf("O dígito %hu está correto na posição %u.\n", password[i], i + 1);
+        } else if (wrong_position_digits[i]) {
+            printf("O dígito %hu está correto, mas na posição errada.\n", password[i]);
+        } else {
+            printf("O dígito %hu não faz parte da senha.\n", password[i]);
+        }
     }
 
-    return 1;
+    printf("\n");
+
+    // Check if there is any incorrect digit to return an error (non-zero value)
+    for (unsigned short i = 0; i < PASSWORD_LENGTH; i++) {
+        if (!correct_digits[i]) {
+            return 1;
+        }
+    }
+
+    // Return zero when the password is correct
+    return 0;
+}
+
+void flush_stdin() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
